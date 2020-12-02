d8478d5fdd7dd3566665859c73be1cbfd69ad6ab
bayazitdeniz <38927220+bayazitdeniz@users.noreply.github.com>
2020-12-02 10:36:37 -0500

    getting rid of eval

--- train_single.sh
+++ train_single.sh
@@ -9,7 +9,7 @@ python3 main_distributed.py --n_display=1 \
        --caption_root=data/small_train_captions \
        --train_csv=data/small_train_videos.csv --video_path=data/training \
        --word2vec_path $PWD/data/word2vec.pth \
-       --eval_video_root=data/small_val_videos --evaluate
+       # --eval_video_root=data/small_val_videos --evaluate
 
 # python main_distributed.py --verbose --n_display=1 \
 #       --multiprocessing-distributed --batch_size=256 \
