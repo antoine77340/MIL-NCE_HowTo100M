7fe45c13b3650da012df46fc42e91e0c7bbc255a
bayazitdeniz <38927220+bayazitdeniz@users.noreply.github.com>
2020-12-02 12:47:46 -0500

    adding val again

--- train_single.sh
+++ train_single.sh
@@ -4,13 +4,13 @@ python3 main_distributed.py --n_display=1 \
        --batch_size=16 --batch_size_val=8 \
        --num_thread_reader=40 --cudnn_benchmark=1 --pin_memory \
        --checkpoint_dir=pmilnce --num_candidates=$2 --lr=0.001 \
-       --warmup_steps=10000 --epochs=100 \
+       --warmup_steps=10000 --epochs=10 \
        --seed=$1 --world-size=1 --rank=0\
        --caption_root=data/small_train_captions \
        --train_csv=data/small_train_videos.csv --video_path=data/training \
        --word2vec_path $PWD/data/word2vec.pth \
        --num_frames=8 --video_size=112 \
-       # --eval_video_root=data/small_val_videos --evaluate
+       --eval_video_root=data/small_val_videos --evaluate
 
 # python main_distributed.py --verbose --n_display=1 \
 #       --multiprocessing-distributed --batch_size=256 \
