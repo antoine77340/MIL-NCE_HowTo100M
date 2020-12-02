b5b81fa083fb8437844466525ddb29066d929750
bayazitdeniz <38927220+bayazitdeniz@users.noreply.github.com>
2020-12-02 10:20:59 -0500

    line breaking typo corrected

--- train_single.sh
+++ train_single.sh
@@ -8,8 +8,8 @@ python3 main_distributed.py --n_display=1 \
        --seed=$1 --world-size=1 --rank=0\
        --caption_root=data/small_train_captions \
        --train_csv=data/small_train_videos.csv --video_path=data/training \
-       --word2vec_path $PWD/data/word2vec.pth
-       --eval_video_root=data/small_val_videos --evaluate \
+       --word2vec_path $PWD/data/word2vec.pth \
+       --eval_video_root=data/small_val_videos --evaluate
 
 # python main_distributed.py --verbose --n_display=1 \
 #       --multiprocessing-distributed --batch_size=256 \
