765988dea8276d6093ca192e974cabf6832d756b
bayazitdeniz <38927220+bayazitdeniz@users.noreply.github.com>
2020-12-02 11:44:46 -0500

    downsizing videos

--- train_single.sh
+++ train_single.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 python3 main_distributed.py --n_display=1 \
-       --batch_size=2 \
+       --batch_size=8 \
        --num_thread_reader=40 --cudnn_benchmark=1 --pin_memory \
        --checkpoint_dir=pmilnce --num_candidates=$2 --lr=0.001 \
        --warmup_steps=10000 --epochs=4 \
@@ -9,6 +9,7 @@ python3 main_distributed.py --n_display=1 \
        --caption_root=data/small_train_captions \
        --train_csv=data/small_train_videos.csv --video_path=data/training \
        --word2vec_path $PWD/data/word2vec.pth \
+       --num_frames=8 --video_size=112
        # --eval_video_root=data/small_val_videos --evaluate
 
 # python main_distributed.py --verbose --n_display=1 \
@@ -17,3 +18,5 @@ python3 main_distributed.py --n_display=1 \
 #       --checkpoint_dir=milnce --num_candidates=4 --resume --lr=0.001 \
 #       --warmup_steps=10000 --epochs=300 --caption_root=path_to_howto_csv
 #       --eval_video_path=.
+
+bash train_single.sh 42 1
