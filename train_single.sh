2447d1ed70d99a14b1bace6938e53987bf5feca5
bayazitdeniz <38927220+bayazitdeniz@users.noreply.github.com>
2020-12-02 11:46:23 -0500

    correcting typo and enlarging training epoch

--- train_single.sh
+++ train_single.sh
@@ -4,7 +4,7 @@ python3 main_distributed.py --n_display=1 \
        --batch_size=8 \
        --num_thread_reader=40 --cudnn_benchmark=1 --pin_memory \
        --checkpoint_dir=pmilnce --num_candidates=$2 --lr=0.001 \
-       --warmup_steps=10000 --epochs=4 \
+       --warmup_steps=10000 --epochs=100 \
        --seed=$1 --world-size=1 --rank=0\
        --caption_root=data/small_train_captions \
        --train_csv=data/small_train_videos.csv --video_path=data/training \
@@ -18,5 +18,3 @@ python3 main_distributed.py --n_display=1 \
 #       --checkpoint_dir=milnce --num_candidates=4 --resume --lr=0.001 \
 #       --warmup_steps=10000 --epochs=300 --caption_root=path_to_howto_csv
 #       --eval_video_path=.
-
-bash train_single.sh 42 1
